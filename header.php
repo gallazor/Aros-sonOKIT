f783393c1acf1e0dcdbad92cdce2d2e2f72ed66a
tommynguye3n <73479424+tommynguye3n@users.noreply.github.com>
2021-05-27 13:02:24 +0200

    login

--- header.php
+++ header.php
@@ -45,7 +45,7 @@ include_once("boostrap.html");
               <a class="nav-link text-dark" href="#" >Beskeder</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link text-dark" href="loginindex.php" >test</a>
+              <a class="nav-link text-dark" href="loginindex.php" >Login</a>
           </li>
       </ul>
   </div>
